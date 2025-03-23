8cdc5b75eb423ae402e44819e1bb13460091e12e
Ted Kesgar <t.kesgar@outlook.com>
2025-03-23 10:41:10 +0700

    Remove typing the response

--- playground/api/index.ts
+++ playground/api/index.ts
@@ -1,25 +1,16 @@
-import { Elysia, t } from 'elysia'
+import { Elysia } from 'elysia'
 import { swagger } from '@elysiajs/swagger'
 
 export default async () => {
-  // NOTE: For some reason Elysia cannot infer the returned type, so we need to
-  // specify the type in response
   const app = new Elysia()
     .get('/hello', () => {
       return 'Hello world!'
-    }, {
-      response: t.String(),
     })
     .get('/api-hello', () => {
       return {
         status: 'ok',
         message: 'Hello world!',
       }
-    }, {
-      response: t.Object({
-        status: t.String(),
-        message: t.String(),
-      }),
     })
     .use(swagger())
 
