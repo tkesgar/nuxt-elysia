4b91edf66a91c123b3f3ccccd432bf3d01692fb8
Ted Kesgar <t.kesgar@outlook.com>
2025-11-19 20:56:44 +0700

    Remove useRuntimeConfig usage as our vitest does not run in Nitro context

--- playground/api/complex-json.ts
+++ playground/api/complex-json.ts
@@ -1,15 +1,12 @@
 import { Elysia } from 'elysia'
 
 export default function complexJsonApp() {
-  const runtimeConfig = useRuntimeConfig()
-
   return new Elysia().get('/complex-json', () => {
     return {
       randomNumber: Math.random(),
       uuid: crypto.randomUUID(),
       date: new Date(),
       buffer: Buffer.from('Hello world!'),
-      appRuntimeConfig: runtimeConfig.app,
     }
   })
 }
